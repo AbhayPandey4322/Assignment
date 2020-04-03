d853cd42657a683838828ad30c16665a4e5b1635
AbhayPandey4322 <abhaypandey5015@gmail.com>
2020-04-03 15:44:39 +0530

    Commit

---
+++ Question2/Question2/Controllers/EmployeeController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Question2.Models;
+
+namespace Question2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> EmpList = new List<Employee>()
+
+            {
+             new Employee {Id = 1, Name = "Abhay", Designation ="Developer",Contact=987456321},
+                new Employee {Id = 2, Name = "Sam", Designation ="Designer",Contact=87956321},
+                new Employee {Id = 3, Name = "Zoya", Designation ="HR",Contact=789456321 },
+                new Employee {Id = 4, Name = "Ram", Designation ="Tester",Contact=789456321 }
+        };
+            ViewBag.EmpData =EmpList;
+            ViewData["Title"]="Employee Details";
+            return View();
+            
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
